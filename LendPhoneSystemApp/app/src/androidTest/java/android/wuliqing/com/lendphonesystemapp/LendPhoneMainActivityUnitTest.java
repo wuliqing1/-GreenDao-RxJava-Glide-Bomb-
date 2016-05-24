4644abfe89707044a9702213d239263d867e90d2
wuliqing <wlq1636073025@163.com>
2016-05-24 17:05:21 +0800

    LendPhoneSystem工程初始化

---
+++ LendPhoneSystemApp/app/src/androidTest/java/android/wuliqing/com/lendphonesystemapp/LendPhoneMainActivityUnitTest.java
@@ -0,0 +1,40 @@
+package android.wuliqing.com.lendphonesystemapp;
+
+import android.support.design.widget.FloatingActionButton;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+/**
+ * Created by 10172915 on 2016/5/24.
+ */
+public class LendPhoneMainActivityUnitTest extends ActivityInstrumentationTestCase2<LendPhoneMainActivity> {
+    LendPhoneMainActivity lendPhoneMainActivity;
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        lendPhoneMainActivity = (LendPhoneMainActivity)getActivity();
+        floatingActionButton = findViewById(R.id.fab);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testWidgetVisible() {
+        assertEquals(true, floatingActionButton.getVisibility() == View.GONE);
+    }
+
+    public LendPhoneMainActivityUnitTest() {
+        super(LendPhoneMainActivity.class);
+    }
+
+    public LendPhoneMainActivityUnitTest(Class<LendPhoneMainActivity> activityClass) {
+        super(activityClass);
+    }
+
+    private <T extends View> T findViewById(int id) {
+        return (T) lendPhoneMainActivity.findViewById(id);
+    }
+}
